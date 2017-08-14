c7095916cf362893b289deeb3ceb5056472f21b8
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-08-14 23:51:03 +0700

    Create method to return all of users

--- ProjectASPWebAPI/Controllers/UsersController.cs
+++ ProjectASPWebAPI/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using ProjectASPWebAPI.Models;
 using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace ProjectASPWebAPI.Controllers
@@ -9,20 +12,44 @@ namespace ProjectASPWebAPI.Controllers
         [HttpGet]
         public IHttpActionResult All()
         {
-            //using (var context = new Context())
-            //{
-            //    context.Users.Add(new User()
-            //    {
-            //        FirstName = "Alvin",
-            //        LastName = "Oktavianus",
-            //        Password = "Halelujah",
-            //        Email = "agentsam1995@gmail.com",
-            //        CreatedAt = DateTime.Today,
-            //        UpdatedAt = DateTime.Today
-            //    });
-            //    context.SaveChanges();
-            //}
-            return Ok();
+            using (var context = new Context())
+            {
+                var users = context.Users.ToList();
+                return Ok(users);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult FindUserById(int id)
+        {
+            using (var context = new Context())
+            {
+                var user = context.Users.Find(id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return Ok(user);
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage InsertNewData([FromBody] User user)
+        {
+            using (var context = new Context())
+            {
+                context.Users.Add(new User()
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Password = user.Password,
+                    CreatedAt = DateTime.Today,
+                    UpdatedAt = DateTime.Today
+                });
+                context.SaveChanges();
+            }
+            return Request.CreateResponse(HttpStatusCode.Created, "OK!");
         }
     }
 }

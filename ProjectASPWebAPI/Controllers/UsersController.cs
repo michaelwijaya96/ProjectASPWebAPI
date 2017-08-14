832717b89ed6f7db6caad3ff4a44ffe14afb660a
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-08-14 21:02:19 +0700

    Create sample controller

---
+++ ProjectASPWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProjectASPWebAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult All()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}

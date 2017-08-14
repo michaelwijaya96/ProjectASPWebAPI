f52615b9916d7448c8d6f5422f62cea2c399159e
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-08-14 21:09:49 +0700

    Create user model

---
+++ ProjectASPWebAPI/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectASPWebAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(50)]
+        public string LastName { get; set; }
+        [Required, MaxLength(60)]
+        public string Password { get; set; }
+        [Required, MaxLength(50)]
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}

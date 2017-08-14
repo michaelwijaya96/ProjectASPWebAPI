c848c6267a319d6b2283a0132299c435e48d2c6f
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-08-14 21:56:08 +0700

    Save current working directory

---
+++ ProjectASPWebAPI/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using ProjectASPWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjectASPWebAPI
+{
+    public class ProjectContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+    }
+}

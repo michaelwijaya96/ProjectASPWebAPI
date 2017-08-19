97ff12672e4b1b2b8015438a942fe211035f037f
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-08-19 17:06:56 +0700

    temp commit

---
+++ ProjectASPWebAPI/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectASPWebAPI.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        [Required, MaxLength(50)]
+        public string UserName { get; set; }
+        [Required, MaxLength(100)]
+        public string UserPassword { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime UpdDate { get; set; }
+    }
+}
